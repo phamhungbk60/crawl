f923823f09364938a4c496a1827960c935c9ae6e
hungpv <phamhungbk60@gmail.com>
2022-06-16 20:36:49 +0700

    MVC pattern tuning

--- Test.php
+++ Test.php
@@ -28,6 +28,25 @@
     }
 ?>
 
+
+<table cellspacing="0" cellpadding="0" class="form">
+    <thead>
+        <tr>
+            <td>Username</td>
+            <td>Email</td>
+            <td>Action</td>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($users as $item){ ?>
+        <tr>
+            <td><?php echo $item['username']; ?></td>
+            <td><?php echo $item['email']; ?></td>
+        </tr>
+        <?php } ?>
+    </tbody>
+</table>
+
 <?php
 class Config {    
     const SERVERNAME = "localhost";
@@ -52,28 +71,35 @@ class Config {
         curl_setopt($ch, CURLOPT_URL, $url);
         $result = curl_exec($ch);
         // $regex_title = '#<h1 class="title-page detail">(.*?)</h1>#si';
-        // $regex_title = '#<h1 class="title .+?">(.*?)</h1>#si';
-        $regex_title = '#<h1 class="title-detail">(.*?)</h1>#si';
+        $regex_title = '#<h1 class="newsFeature__header-title mt-20">(.*?)</h1>#si';
+        // $regex_title = '#<h1 class="title-detail">(.*?)</h1>#si';
         preg_match($regex_title, $result, $title);
 
         // Get Data description
         // $regex_description = '#<h2 class="singular-sapo">(.*?)</h2>#si';
-        // $regex_description = '#<div class="bold ArticleLead"><p>(.*?)</p></div>#si';
-        $regex_description = '#<p class="description">(.*?)</p>#si';
+        $regex_description = '#<div class="newFeature__main-textBold">(.*?)</div>#si';
+        // $regex_description = '#<p class="description">(.*?)</p>#si';
         preg_match($regex_description, $result, $description);
 
         // Get Data Date
-        
-        // $regex_date = '#<span class="ArticleDate">(.*?)</span>#si';
-        $regex_date = '#<span class="date">(.*?)</span>#si';
+        $regex_date = '#<time class="author-time" .+?>(.*?)</time>#si';
+        // $regex_date = '#<span>(.*?)</span>#si';
+        // $regex_date = '#<span class="date">(.*?)</span>#si';
         preg_match($regex_date, $result, $date);
 
         // Get Data Details
         // $regex_details = '#<p>(.*?)</p>#si';
-        // $regex_details = '#<p class="t-j">(.*?)</p>#si';
-        $regex_details = '#<p class="Normal">(.*?)</p>#si';
+        $regex_details = '#<p>(.*?)</p>#si';
+        // $regex_details = '#<p class="Normal">(.*?)</p>#si';
         
         preg_match_all($regex_details, $result, $details);
+
+        $sqls = sprintf("Select * From data");
+        if ($connect->query($sqls) === TRUE) {
+            echo "New record created successfully";
+          } else {
+            echo "Error: " . $sqls . "<br>" . $connect->error;
+          }
 
         $sql = sprintf("INSERT INTO Data (title, description, date, details) 
         VALUES ('%s', '%s', '%s', '%s')", str_replace("'", "", $title[1]), str_replace("'", "", $description[1]), $date[1], str_replace("'", "", implode (" ",$details[1])));
@@ -86,5 +112,6 @@ class Config {
         $connect->close();
     }
 }
+
 $CONSO = new Config;
 $CONSO->connect();
